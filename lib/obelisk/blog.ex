a03d2577a95b9c33ff9ed0a52036e998d3d81cdf
Jonathan Storm <the.jonathan.storm@gmail.com>
2016-03-02 15:04:09 -0600

    Improve clarity in Obelisk.Blog
      * Broke _compile_index/3 and write_index_page/4 into smaller pieces
      * Replaced split/reverse/join acrobatics with Path manipulations
      * Expanded abbreviated variable names

--- lib/obelisk/blog.ex
+++ lib/obelisk/blog.ex
@@ -25,89 +25,142 @@ defmodule Obelisk.Blog do
     end
   end
 
+  defmodule IndexPage do
+    defstruct(
+      next_page: nil,
+      previous_page: nil,
+      content: nil,
+      document: nil
+    )
+  end
+
   def compile_index(posts, store) do
-    Obelisk.Config.config
-    |> Dict.get(:blog_index)
-    |> make_path
+    make_path get_blog_index
+
+    layouts = Obelisk.Store.get_layouts store
 
     posts
     |> Enum.sort(get_sort_function)
-    |> _compile_index(store)
+    |> _compile_index(layouts, get_posts_per_page, 1)
   end
 
-  defp _compile_index([], _, _), do: nil
-  defp _compile_index(posts, store, page_num \\ 1) do
-    { ppp, _ } = Integer.parse Obelisk.Config.config.posts_per_page
-    { c, r } = Enum.split(posts, ppp)
-    write_index_page c, page_num, last_page?(r), store
-    _compile_index r, store, page_num + 1
+  defp _compile_index([], _, _, _), do: nil
+  defp _compile_index(posts, layouts, posts_per_page, page_number) do
+    path = html_filename page_number
+
+    { current, remaining } = Enum.split posts, posts_per_page
+
+    previous = previous_page page_number
+    next = next_page page_number, is_last_page(remaining)
+
+    current
+    |> posts_to_index_page(previous, next, layouts)
+    |> write_index_page(path)
+
+    _compile_index remaining, layouts, posts_per_page, page_number + 1
   end
 
-  defp write_index_page(posts, page_num, last_page, store) do
-    templates = Obelisk.Store.get_layouts(store)
-    { layout, layout_renderer } = templates.layout
-    { index, index_renderer } = templates.index
-    File.write(html_filename(page_num),
-      Obelisk.Renderer.render(layout, [js: Obelisk.Assets.js, css: Obelisk.Assets.css, content: Obelisk.Renderer.render(index, [prev: previous_page(page_num), next: next_page(page_num, last_page), content: posts ], index_renderer)], layout_renderer))
+  defp write_index_page(index_page, path) do
+    File.write path, index_page.document
   end
 
-  defp last_page?([]), do: true
-  defp last_page?(_),  do: false
+  defp is_last_page([]), do: true
+  defp is_last_page(_),  do: false
 
   def html_filename(page_num) do
-    Obelisk.Config.config
-    |> Dict.get(:blog_index)
+    get_blog_index
     |> with_index_num(page_num)
     |> build_index_path
   end
 
-  defp with_index_num(nil, 1), do: "index.html"
-  defp with_index_num(nil, page_num), do: "index#{page_num}.html"
-  defp with_index_num(c, 1), do: c
-  defp with_index_num(c, page_num) do
-    [ext|reverse_path] = c
-    |> String.split(".")
-    |> Enum.reverse
+  defp with_index_num(index_path_with_filename, 1), do: index_path_with_filename
+  defp with_index_num(index_path_with_filename, page_number) do
+    root = Path.rootname index_path_with_filename
+    extension = Path.extname index_path_with_filename
 
-    p = reverse_path
-    |> Enum.reverse
-    |> Enum.join
-
-    p <> to_string(page_num) <> "." <> ext
+    root <> to_string(page_number) <> extension
   end
 
   defp make_path(nil), do: nil
   defp make_path(path) do
-    path
-    |> String.split("/")
-    |> Enum.reverse
-    |> _make_path
+    case get_build_path(path) do
+      "./build" ->
+        nil
+
+      build_path ->
+        File.mkdir_p build_path
+    end
   end
 
-  defp _make_path([_filename|[]]), do: nil
-  defp _make_path([_filename|reverse_path]) do
-    [reverse_path] ++ ["build", "."] # Will reverse to ./build/path/unreversed
-    |> Enum.reverse
-    |> Enum.join("/")
-    |> File.mkdir_p
+  defp get_build_path(path_with_filename) do
+    path = Path.dirname path_with_filename
+
+    "./build"
+    |> Path.join(path)
+    |> Path.rootname
   end
 
-  defp build_index_path(path), do: "./build/" <> path
+  defp render_content(index_page, posts, layout) do
+    params =
+      [ prev: index_page.previous_page,
+        next: index_page.next_page,
+        content: posts
+      ]
+
+    %IndexPage{
+      index_page | content: Obelisk.Renderer.render(layout, params)
+    }
+  end
+
+  defp render_assets(index_page, js_assets, css_assets, layout) do
+    params =
+      [ js: js_assets,
+        css: css_assets,
+        content: index_page.content
+      ]
+
+    %IndexPage{
+      index_page | document: Obelisk.Renderer.render(layout, params)
+    }
+  end
+
+  defp posts_to_index_page(posts, previous, next, layouts) do
+		js_assets = Obelisk.Assets.js
+		css_assets = Obelisk.Assets.css
+
+    %IndexPage{next_page: next, previous_page: previous}
+    |> render_content(posts, layouts.index)
+    |> render_assets(js_assets, css_assets, layouts.layout)
+  end
+
+  defp build_index_path(path), do: Path.join("./build", path)
+
+  defp get_site_config do
+    Obelisk.Config.config
+  end
+
+  defp get_blog_index do
+    get_site_config[:blog_index] || "index.html"
+  end
+
+  defp get_posts_per_page do
+    (get_site_config[:posts_per_page] || "10")
+    |> Integer.parse
+    |> elem(0)
+  end
 
   defp build_link(path, text), do: "<a href=\"#{path}\">#{text}</a>"
 
-  def previous_page(1),        do: ""
+  def previous_page(1),       do: ""
   def previous_page(page_num) do
-    Obelisk.Config.config
-    |> Dict.get(:blog_index)
+    get_blog_index
     |> with_index_num(page_num - 1)
     |> build_link("Previous Page")
   end
 
-  def next_page(_page_num, true),  do: ""
-  def next_page(page_num,  false) do
-    Obelisk.Config.config
-    |> Dict.get(:blog_index)
+  def next_page(_page_num, _last_page? = true), do: ""
+  def next_page( page_num, _last_page? = false) do
+    get_blog_index
     |> with_index_num(page_num + 1)
     |> build_link("Next Page")
   end
